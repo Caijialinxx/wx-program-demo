b89952b5197fd86a8671cb3e2600e4739bf6cde8
Caijialinxx <caijialinxx@foxmail.com>
2018-09-14 20:11:06 +0800

    实现账号密码登录

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,4 +1,5 @@
 const app = getApp()
+import { logIn } from '../../utils/leancloud.js'
 
 Page({
   getUserInfo: function (e) {
@@ -7,4 +8,42 @@ Page({
       delta: 1
     })
   },
+  verifyInfo: function (e) {
+    let { email, password } = e.detail.value,
+      index_at = email.indexOf('@'),
+      index_point = email.indexOf('.'),
+      length_strAfterPoint = email.substr(index_point + 1).length
+
+    if (email.trim() === '') {
+      wx.showModal({
+        title: '邮箱不能为空',
+        content: '请输入您注册的邮箱，如未注册，请先注册或使用微信快捷登录。',
+        showCancel: false,
+      })
+    } else if (password.trim() === '') {
+      wx.showModal({
+        title: '密码不能为空',
+        content: '',
+        showCancel: false,
+      })
+    } else if (index_at < 1 || index_point < index_at + 2 || length_strAfterPoint < 2) {
+      wx.showModal({
+        title: '电子邮箱无效',
+        content: '请输入正确的电子邮箱地址！',
+        showCancel: false,
+      })
+    } else {
+      logIn(email, password, (user) => {
+        app.globalData.userInfo = user
+        wx.navigateBack({
+          delta: 1
+        })
+      }, (error) => {
+        wx.showToast({
+          title: error,
+          icon: 'none'
+        })
+      })
+    }
+  }
 })
---
+++ utils/leancloud.js
@@ -0,0 +1,33 @@
+const AV = require('../libs/av-weapp-min.js')
+const { appId, appKey } = require('../private.js')
+AV.init({ appId: appId, appKey: appKey })
+
+function logIn(email, password, successFn, errorFn) {
+  AV.User.logIn(email, password).then((loggedInUser) => {
+    let user = getUserInfo(loggedInUser)
+    successFn.call(undefined, user)
+  }, (error) => {
+    switch (error.code) {
+      case -1:
+        errorFn.call(undefined, `请求被终止，请检查网络是否正确连接！`)
+        return;
+      case 210:
+        errorFn.call(undefined, `账号或密码错误！请检查！`)
+        return;
+      case 211:
+        errorFn.call(undefined, `账号不存在！如果未注册请先注册。`)
+        return;
+      case 216:
+        errorFn.call(undefined, `电子邮箱未通过验证，请先验证再登录！验证邮件已发送至您的邮箱（${email}），请转至邮箱查收并进行验证！`)
+        return;
+      case 219:
+        errorFn.call(undefined, `登录失败次数超过限制，请稍候再试！或者通过忘记密码重设密码。`)
+        return;
+      default:
+        errorFn.call(undefined, `${error.message}`)
+        return;
+    }
+  })
+}
+
+module.exports = { logIn }
