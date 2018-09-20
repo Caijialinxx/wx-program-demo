7613d7a1d2d2cae31996c5bfd3c32e61eeecb613
Caijialinxx <caijialinxx@foxmail.com>
2018-09-20 18:29:02 +0800

    验证邮箱

--- pages/profile/edit/profile-edit.js
+++ pages/profile/edit/profile-edit.js
@@ -62,6 +62,69 @@ Page({
       })
     })
   },
+  showEmailSettingWindow: function () {
+    this.setData({ linkEmailNeeded: true })
+  },
+  cancelLinkEmail: function () {
+    this.setData({ linkEmailNeeded: false })
+  },
+  nextSetPassword: function () {
+    this.setData({ transformValue: 'translateX(-50%)' })
+  },
+  previousSetEmail: function () {
+    this.setData({ transformValue: 'translateX(0%)' })
+  },
+  linkEmail: function ({ detail: { value: { email, password } } }) {
+    let index_at = email.indexOf('@'),
+      index_point = email.indexOf('.'),
+      length_strAfterPoint = email.substr(index_point + 1).length
+
+    if (email.indexOf(' ') > -1 || email === '') {
+      wx.showModal({
+        title: '邮箱不能为空或含有空格！',
+        showCancel: false,
+      })
+    } else if (index_at < 1 || index_point < index_at + 2 || length_strAfterPoint < 2) {
+      wx.showModal({
+        title: '邮箱格式错误！',
+        showCancel: false,
+      })
+    } else if (password.indexOf(' ') > -1 || password === '') {
+      wx.showModal({
+        title: '密码不能为空或含有空格！',
+        showCancel: false,
+        success: () => {
+          this.setData({
+            password: '',
+            password_confirm: ''
+          })
+        }
+      })
+    } else {
+      let user = AV.User.current()
+      user.setEmail(email).setPassword(password).save().then((user) => {
+        console.log(user)
+        app.globalData.userInfo.email = email
+        this.setData({
+          userInfo: app.globalData.userInfo,
+          linkEmailNeeded: false,
+          transformValue: 'translateX(0%)'
+        })
+        console.log(this.data)
+        wx.showModal({
+          title: '验证邮件已发送',
+          content: `验证邮件已发送至您的邮箱【${email}】，请转至邮箱通过验证即可邮箱和密码登录使用。`,
+          showCancel: false,
+          confirmText: '知道了'
+        })
+      }, (error) => {
+        wx.showToast({
+          title: error.code.toString(),
+          icon: 'none'
+        })
+      })
+    }
+  },
   linkWeChat: function ({ detail }) {
     if (detail.errMsg === 'getUserInfo:ok') {
       linkWeChat().then(() => {
